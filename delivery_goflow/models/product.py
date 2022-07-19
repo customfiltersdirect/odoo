5f784485c596e81d8e78f81a0051df095bffedf5
Ashish-Techversant <ashish.thomas@techversantinfo.com>
2022-07-19 21:40:35 +0530

    [IMP[product tags is fiing

--- delivery_goflow/models/product.py
+++ delivery_goflow/models/product.py
@@ -23,49 +23,30 @@ class ProductProduct(models.Model):
         headers = {
             'X-Beta-Contact': self.env.user.partner_id.email
         }
-        try:
-            result = requests.get(url, auth=BearerAuth(goflow_token), headers=headers)
-            goflow_api = result.json()
-            products = goflow_api["data"]
-            while goflow_api["next"]:
-                goflow_api = requests.get(goflow_api["next"], auth=BearerAuth(goflow_token), headers=headers).json()
-                products.extend(goflow_api["data"])
 
-            for product in products:
-                prod_name = product['details']['name']
-                if prod_name:
-                    goflow_id = product['id']
-                    check_if_product_exists = self.search([('goflow_id','=',goflow_id)])
-                    goflow_item_no = product['item_number']
-                    goflow_tags_json = product["tags"]
-                    total_tags = self.env['goflow.product.tag'].search([])
-                    allowed_tags = self.env['goflow.product.tag'].search([('sync_products','=',True)]).mapped('goflow_id')
-                    goflow_tags = []
-                    for tag in goflow_tags_json:
-                        goflow_tags.append(tag['id'])
-                    if len(allowed_tags) == len(total_tags):
-                        if not check_if_product_exists:
-                            self.create({'name': prod_name, 'goflow_id': goflow_id, 'goflow_item_no': goflow_item_no,'company_id': company_for_glow and company_for_glow.id or False})
-                    elif not set(allowed_tags).isdisjoint(goflow_tags):
-                        if not check_if_product_exists:
-                            self.create({'name':prod_name,'goflow_id':goflow_id,'goflow_item_no':goflow_item_no,'company_id': company_for_glow and company_for_glow.id or False})
-        except:
-            pass
+        result = requests.get(url, auth=BearerAuth(goflow_token), headers=headers)
+        goflow_api = result.json()
+        products = goflow_api["data"]
+        # while goflow_api["next"]:
+        #     goflow_api = requests.get(goflow_api["next"], auth=BearerAuth(goflow_token), headers=headers).json()
+        #     products.extend(goflow_api["data"])
 
+        for product in products:
+            prod_name = product['details']['name']
 
-
-# import requests #to make TMDB API calls
-#
-# #Discover API url filtered to movies >= 2004 and containing Drama genre_ID: 18
-# discover_api = 'https://api.themoviedb.org/3/discover/movie?
-# api_key=['my api key']&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&primary_release_year=>%3D2004&with_genres=18'
-#
-# discover_api = requests.get(discover_api).json()
-# most_popular_films = discover_api["results"]
-# while discover_api["next_url"]:
-#     discover_api = requests.get(discover_api["next_url"]).json()
-#     most_popular_films.extend(discover_api["results"])
-#
-# #printing movie_id and movie_title by popularity desc
-# for i, film in enumerate(most_popular_films):
-#     print(i, film['id'], film['title'])
+            if prod_name:
+                goflow_id = product['id']
+                check_if_product_exists = self.search([('goflow_id','=',goflow_id)])
+                goflow_item_no = product['item_number']
+                goflow_tags_json = product["tags"]
+                total_tags = self.env['goflow.product.tag'].search([])
+                allowed_tags = self.env['goflow.product.tag'].search([('sync_products','=',True)])
+                goflow_tags = []
+                for tag in goflow_tags_json:
+                    goflow_tags.append(tag['id'])
+                if len(allowed_tags) == len(total_tags):
+                    if not check_if_product_exists:
+                        self.create({'name': prod_name, 'goflow_id': goflow_id, 'goflow_item_no': goflow_item_no,'company_id': company_for_glow and company_for_glow.id or False})
+                elif not set(allowed_tags.mapped('goflow_id')).isdisjoint(goflow_tags):
+                    if not check_if_product_exists:
+                        self.create({'name':prod_name,'goflow_id':goflow_id,'goflow_item_no':goflow_item_no,'company_id': company_for_glow and company_for_glow.id or False})
