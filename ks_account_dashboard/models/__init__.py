3b69d4e88f6a5bb141a4c90303a10d67de17eca1
cybrosys <cybroodoo@gmail.com>
2023-03-10 23:03:39 +0530

    commit for 3 ninja template

---
+++ ks_account_dashboard/models/__init__.py
@@ -0,0 +1,2 @@
+from . import ks_dashboard_fiscal_year_template
+from . import ks_dashboard_fiscal_item
---
+++ ks_sale_dashboard_ninja/__manifest__.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Sales Dashboard Ninja',
+
+	'summary': """
+Sales Dashboard,Sales Dashboard Ninja,Sales Odoo Dashboard,Responsive Sales Dashboard,Sales dashboard ninja,Odoo 12 dashboard ,odoo 13 dashboard,custom odoo dashboard,Modern Odoo Dashboards,Odoo 15 Dashboard,odoo 14 dashboard
+""",
+
+	'description': """
+Dashboard Ninja v15.0,
+        Odoo Dashboard,
+        Dashboard,
+        Odoo apps,
+        Dashboard app,
+        HR Dashboard,
+        Sales Dashboard,
+        inventory Dashboard,
+        Lead Dashboard,
+        Opportunity Dashboard,
+        CRM Dashboard,
+        POS,
+        POS Dashboard,
+        Connectors,
+        Web Dynamic,
+        Report Import/Export,
+        Date Filter,
+        HR,
+        Sales,
+        Theme,
+        Tile Dashboard,
+        Dashboard Widgets,
+        Dashboard Manager,
+        Debranding,
+        Customize Dashboard,
+        Graph Dashboard,
+        Charts Dashboard,
+        Invoice Dashboard,
+        Project management,
+        ksolves,
+        ksolves apps,
+        Ksolves India Ltd.,
+        Sales Dashboards,
+        Best Sales Dashboards,
+        Sale Orders,
+        Customer Dashboard,
+        Partners Dashboard,
+        Top Customers Dashboard,
+        Invoice Dashboard,
+        V12 Dashboard,
+        Dashboard v12.0,
+        Odoo Dashboard v12.0,
+        Sales Dashboard v12.0,
+        Odoo Sales Dashboards,
+        Best Sales Dashboard Apps,
+        Best Dashboard Apps,
+        Best Odoo Apps,
+        Dashboard For Sales,
+        Odoo Dashboard apps,
+        Best Odoo Dashboard Apps,
+        Dashboard apps,
+        Dashboards for Sales,
+        HR Dashboard Apps,
+        Sales Dashboard Apps,
+        inventory Dashboard Apps,
+        Lead Dashboards,
+        Opportunity Dashboards,
+        CRM Dashboards,
+        Best POS Apps,
+        POS Dashboards,
+        Sales Connectors,
+        Web Dynamic Apps,
+        Report Import/Export,
+        Date Filter Apps,
+        Tile Dashboard Apps,
+        Dashboard Widgets,
+        Dashboard Manager Apps,
+        Debranding Apps,
+        Customize Dashboard Apps
+        Graph Dashboard Apps,
+        Charts Dashboard Apps,
+        Invoice Dashboard Apps,
+        Project management Apps
+""",
+
+	'author': 'Ksolves India Ltd.',
+
+	'license': 'OPL-1',
+
+	'currency': 'EUR',
+
+	'price': '27.99',
+
+	'website': 'https://store.ksolves.com/',
+
+	'maintainer': 'Ksolves India Ltd.',
+
+	'live_test_url': '',
+
+	'category': 'Tools',
+
+	'support': 'sales@ksolves.com',
+
+	'version': '15.0.1.0.0',
+
+	'images': ['static/description/ks_sales_banner.jpg'],
+
+	'depends': ['ks_dashboard_ninja', 'sale', 'sale_management'],
+
+	'data': ['data/ks_sales_data.xml'],
+}
