4011a4b000208bd29019762c2eba50060b1118cc
wsalgado <110246421+willians-salgado@users.noreply.github.com>
2024-06-11 19:48:34 -0400

    [ADD] mrp_bom_attribute_match

---
+++ mrp_bom_attribute_match/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "BOM Attribute Match",
+    "category": "Manufacturing",
+    "author": "Ilyas, Ooops, Odoo Community Association (OCA)",
+    "summary": "Dynamic BOM component based on product attribute",
+    "depends": ["mrp"],
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/manufacture",
+    "data": [
+        "views/mrp_bom_views.xml",
+    ],
+}
