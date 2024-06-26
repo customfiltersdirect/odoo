d801a4b7a67cc791b5218aee58e20eb7fc3c0d7c
M-Sharjeel-Skyrocket <m.sharjeel@skyrocket.com.pk>
2024-06-27 01:31:41 +0500

    Task : Fix New Fields in Product.Product point no 2
    URL: https://portalold.skyrocket.com.pk/web#id=392&cids=1&menu_id=377&action=775&model=project.task&view_type=form
    
    modul: sr_x_custom_filter_direct_ext
    Remarks: Show Internal Reference and Product name with error on duplicate

--- sr_x_custom_filter_direct_ext/model/product_product.py
+++ sr_x_custom_filter_direct_ext/model/product_product.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
@@ -6,3 +7,27 @@ class ProductProduct(models.Model):
     # ASIN = fields.Char(string="ASIN")
     # go_flow_item_id = fields.Char(string='GO FLow Item ID')
     # go_flow_item_number = fields.Char(string='GO Flow Item Nummber')
+
+    @api.constrains('goflow_id_var')
+    def _goflow_id_var(self):
+        if self.goflow_id_var != False:
+            result = self.env['product.product'].search([('goflow_id_var', '=', self.goflow_id_var),('id', '!=', self.id)], limit=1)
+            if result:
+                match_rec = self.env['product.product'].search([('goflow_id_var', '=', self.goflow_id_var)], limit=1)
+                raise ValidationError("The Operation cannot be completed: this Goflow ID has already assiigned to [" + str(match_rec.default_code) + "] [" + match_rec.name+ "]")
+
+    @api.constrains('goflow_item_no_var')
+    def _goflow_item_no_var(self):
+        if self.goflow_id_var != False:
+            result = self.env['product.product'].search([('goflow_item_no_var', '=', self.goflow_item_no_var),('id', '!=', self.id)], limit=1)
+            if result:
+                match_rec = self.env['product.product'].search([('goflow_item_no_var', '=', self.goflow_item_no_var)], limit=1)
+                raise ValidationError("The Operation cannot be completed: this Goflow Item No has already assiigned to [" + str(match_rec.default_code) + "] [" + match_rec.name + "]")
+
+    @api.constrains('asin_var')
+    def _asin_var(self):
+        if self.goflow_id_var != False:
+            result = self.env['product.product'].search([('asin_var', '=', self.asin_var),('id', '!=', self.id)], limit=1)
+            if result:
+                match_rec = self.env['product.product'].search([('asin_var', '=', self.asin_var)], limit=1)
+                raise ValidationError("The Operation cannot be completed: this Asin ID has already assiigned to [" + str(match_rec.default_code) + "] [" + match_rec.name+ "]")
