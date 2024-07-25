2b9408778532a3fcc87e6072ccff498fc63bb5bd
Hamza khattak <hamza.khattak@skyrocket.com.pk>
2024-07-26 00:55:40 +0500

    Updated Error Message for goflows import shipped orders

--- delivery_goflow/models/goflow_email_action.py
+++ delivery_goflow/models/goflow_email_action.py
@@ -20,7 +20,7 @@ class SaleOrder(models.Model):
             # self.send_success_email()
         except Exception as e:
             error_message = traceback.format_exc()
-            self.send_error_email(error_message)
+            self.send_error_email_import_shipped_order(error_message)
             raise
 
     # without cc email modification
@@ -59,6 +59,24 @@ class SaleOrder(models.Model):
 
     # with cc emails modification
     def send_error_email(self, error_message):
+        email_to = 'odooerrors@customfiltersdirect.com'
+        email_cc = ['hamza.khattak@skyrocket.com.pk','aadeel@skyrocket.com.pk','kashif.ali@skyrocket.com.pk']  # List of CC email addresses
+        subject = 'Error Notification'
+        body_html = f"""
+            <p>An error occurred in the scheduled action:</p>
+            <pre>{error_message}</pre>
+        """
+        mail_values = {
+            'subject': subject,
+            'body_html': body_html,
+            'email_to': email_to,
+            'email_cc': ','.join(email_cc),
+            'email_from': self.env.user.email or 'no-reply@example.com',
+        }
+        mail = self.env['mail.mail'].create(mail_values)
+        mail.send()
+    
+    def send_error_email_import_shipped_order(self, error_message):
         email_to = 'odooerrors@customfiltersdirect.com'
         email_cc = ['hamza.khattak@skyrocket.com.pk','aadeel@skyrocket.com.pk','kashif.ali@skyrocket.com.pk']  # List of CC email addresses
         subject = 'Error Notification'
